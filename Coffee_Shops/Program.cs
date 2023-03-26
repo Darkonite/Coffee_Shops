827c29776eeddc75d10af4bb1e96369a26b7dcac
Cosmin Spataru <cosmin.spataru01@gmail.com>
2023-03-26 21:00:29 +0300

    Code improved

--- Coffee_Shops/Program.cs
+++ Coffee_Shops/Program.cs
@@ -1,102 +1,87 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Net;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
-
-
-//public struct Coffee_Shop {
-//   public string[] name;
-//    float[] x;
-//    float[] y;
-//}
 
 namespace Coffee_Shops
 {
     internal class Program
     {
+
+        public class Coffee_Shop
+        {
+            string name;
+            double xCoord;
+            double yCoord;
+            public Coffee_Shop(string name, double xCoord, double yCoord)
+            {
+                this.name = name;
+                this.xCoord = xCoord;
+                this.yCoord = yCoord;
+
+            }
+
+            public string GetName() { return name; }
+            public double GetXCoord() { return xCoord; }
+            public double GetYCoord() { return yCoord; }
+
+        }
         static void Main(string[] args)
         {
-            // de schimbat in dictionary dupa implementare
-            Dictionary<string, List<double>> dictionary = new Dictionary<string, List<double>>();
-            var path = @"C:\Users\Cosmin\source\repos\Coffee_Shops\Coffee_Shops\coffee_shops_Coding_C.csv";
-            using (var reader = new StreamReader(path))
+
+            if (args.Length == 0 || args.Length == 1)
             {
-                List<string> CoffeeShopName = new List<string>();//// se va sterge la final de implementare dictionary
-                List<double> xCoordinates = new List<double>(); // se va sterge la final de implementare dictionary
-                List<double> yCoordinates = new List<double>();// se va sterge la final de implementare dictionary
-                List<double> coordinates = new List<double>();
-                while (!reader.EndOfStream)
+                Console.WriteLine("Bun venit! Te rog introdu coordonatele tale pentru a rula programul.");
+                Console.WriteLine("Datele se vor introduce sub forma \"47.6\" \"-122.4\". ");
+            }
+            else
+            {
+                var path = @"C:\Users\Cosmin\source\repos\Coffee_Shops\Coffee_Shops\coffee_shops_Coding_C.csv";
+                using (var reader = new StreamReader(path))
                 {
-                    var line = reader.ReadLine();
-                    var values = line.Split(',');
-
-                    CoffeeShopName.Add(values[0]);//se va sterge
-                    xCoordinates.Add(double.Parse(values[1])); // se va sterge
-                    yCoordinates.Add(double.Parse(values[2])); // se va sterge
-                    coordinates.Add(double.Parse(values[1]));
-                    coordinates.Add(double.Parse(values[2]));
-
-                    //coordinates[0] = (double.Parse(values[1]));
-                   // coordinates[1]=(double.Parse(values[2]));
-
-                    dictionary.Add(values[0],coordinates.GetRange(coordinates.Count-2,2));
-                   // coordinates.RemoveRange(0, 2);
-                       
-                }
-
-                foreach (string key in dictionary.Keys)
-                {
-                    Console.Write(key+"  ");
-                    foreach (var val in dictionary[key])
+                    Coffee_Shop[] coffee_shops = new Coffee_Shop[6];
+                    int index = 0;
+                    while (!reader.EndOfStream)
                     {
-                        Console.Write(val+" ");
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+                        coffee_shops[index] = new Coffee_Shop(values[0], double.Parse(values[1]), double.Parse(values[2]));
+                        index++;
                     }
-                    Console.WriteLine();
-                }
 
-                //introducerea coordonatelor
-                Console.WriteLine("Bun venit! Te rog introdu coordonatele tale:");
-                Console.Write("x:");
-                var x=Console.ReadLine();
-                Console.Write("y:");
-                var y=Console.ReadLine();
-
-                //initializarea unui vector pentru distante cu numarul de cafenele stocate
-                double[] distances=new double[CoffeeShopName.Count];
-                for (int i = 0; i < xCoordinates.Count; i++)//dictionary.count merge aici
-                {
-                    double valx = Math.Pow(double.Parse(x) - xCoordinates[i],2);
-                    double valy = Math.Pow(double.Parse(y) - yCoordinates[i], 2);
-                    double val = valx + valy;
-                    distances[i] = Math.Round(Math.Sqrt(val),4);
-                }
-                
-                //sortarea vectorului crescator si sortarea si a numelor ~ greseala ca nu se sorteaza toate liste
-                //schimbare in dictionary dupa implementare
-                for (int i = 0; i < distances.Length - 1; i++)
-                    for (int j = i + 1; j < distances.Length; j++)
+                    List<double> distance = new List<double>();
+                    foreach (var coord in coffee_shops)
                     {
-                        if (distances[i] > distances[j])
+                        var valx = Math.Pow(double.Parse(args[0]) - coord.GetXCoord(), 2);
+                        var valy = Math.Pow(double.Parse(args[1]) - coord.GetYCoord(), 2);
+                        double val = valx + valy;
+                        distance.Add(Math.Round(Math.Sqrt(val), 4));
+                    }
+                    for (int i = 0; i < distance.Count - 1; i++)
+                        for (int j = i + 1; j < distance.Count; j++)
                         {
-                            double aux = distances[i];
-                            distances[i] = distances[j];    
-                            distances[j] = aux;
-                            string aux2 = CoffeeShopName[i];
-                            CoffeeShopName[i] = CoffeeShopName[j];
-                            CoffeeShopName[j] = aux2;
+                            if (distance[i] > distance[j])
+                            {
+                                double aux = distance[i];
+                                distance[i] = distance[j];
+                                distance[j] = aux;
+                                Coffee_Shop caf = coffee_shops[i];
+                                coffee_shops[i] = coffee_shops[j];
+                                coffee_shops[j] = caf;
+                            }
                         }
+                    int firstThreeShops = 0;
+                    foreach (var caf in coffee_shops)
+                    {
+                        Console.WriteLine(caf.GetName() + "   " + distance[firstThreeShops]);
+                        firstThreeShops++;
+                        if (firstThreeShops == 3) break;
                     }
-                for (int i = 0; i < 3; i++)
-                { Console.WriteLine(CoffeeShopName[i]+ "   "+ distances[i] + "  "); }
+                }
+
+
             }
 
         }
     }
-    }
+}
 
