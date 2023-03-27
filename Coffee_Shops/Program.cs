f35fd49647d2d3338674e502ed30f7b64b2d7045
Cosmin Spataru <cosmin.spataru01@gmail.com>
2023-03-27 15:03:23 +0300

    Reading data from url.

--- Coffee_Shops/Program.cs
+++ Coffee_Shops/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace Coffee_Shops
 {
@@ -25,63 +26,69 @@ namespace Coffee_Shops
             public double GetYCoord() { return yCoord; }
 
         }
+
         static void Main(string[] args)
         {
 
-            if (args.Length == 0 || args.Length == 1)
+            if (args.Length < 2)
             {
-                Console.WriteLine("Bun venit! Te rog introdu coordonatele tale pentru a rula programul.");
-                Console.WriteLine("Datele se vor introduce sub forma \"47.6\" \"-122.4\". ");
+                Console.WriteLine("Bun venit! Te rog introdu date pentru a rula programul.");
+                Console.WriteLine("Datele se vor introduce sub forma x: \"47.6\" y: \"-122.4\" url: \"link\". ");
             }
             else
             {
-                var path = @"C:\Users\Cosmin\source\repos\Coffee_Shops\Coffee_Shops\coffee_shops_Coding_C.csv";
-                using (var reader = new StreamReader(path))
+                var response = new WebClient().DownloadString(args[2]);
+                var numberOfEnters = Regex.Matches(response, @"\n").Count;
+                string[] datas = response.Split(new char[] { ',', '/', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                int z = 0, index = 0;
+                Coffee_Shop[] coffee_shops = new Coffee_Shop[numberOfEnters+1];
+                List<string> shop_data = new List<string>();
+                foreach (string data in datas)
                 {
-                    Coffee_Shop[] coffee_shops = new Coffee_Shop[6];
-                    int index = 0;
-                    while (!reader.EndOfStream)
+                    if (!string.IsNullOrWhiteSpace(data))
                     {
-                        var line = reader.ReadLine();
-                        var values = line.Split(',');
-                        coffee_shops[index] = new Coffee_Shop(values[0], double.Parse(values[1]), double.Parse(values[2]));
-                        index++;
-                    }
-
-                    List<double> distance = new List<double>();
-                    foreach (var coord in coffee_shops)
-                    {
-                        var valx = Math.Pow(double.Parse(args[0]) - coord.GetXCoord(), 2);
-                        var valy = Math.Pow(double.Parse(args[1]) - coord.GetYCoord(), 2);
-                        double val = valx + valy;
-                        distance.Add(Math.Round(Math.Sqrt(val), 4));
-                    }
-                    for (int i = 0; i < distance.Count - 1; i++)
-                        for (int j = i + 1; j < distance.Count; j++)
+                        shop_data.Add(data);
+                        z++;
+                        if (z == 3)
                         {
-                            if (distance[i] > distance[j])
-                            {
-                                double aux = distance[i];
-                                distance[i] = distance[j];
-                                distance[j] = aux;
-                                Coffee_Shop caf = coffee_shops[i];
-                                coffee_shops[i] = coffee_shops[j];
-                                coffee_shops[j] = caf;
-                            }
+                            coffee_shops[index] = new Coffee_Shop(shop_data[0], double.Parse(shop_data[1]), double.Parse(shop_data[2]));
+                            z = 0;
+                            shop_data.RemoveRange(0, 3);
+                            index++;
+
                         }
-                    int firstThreeShops = 0;
-                    foreach (var caf in coffee_shops)
-                    {
-                        Console.WriteLine(caf.GetName() + "   " + distance[firstThreeShops]);
-                        firstThreeShops++;
-                        if (firstThreeShops == 3) break;
                     }
                 }
 
-
+                List<double> distance = new List<double>();
+                foreach (var coord in coffee_shops)
+                {
+                    var valx = Math.Pow(double.Parse(args[0]) - coord.GetXCoord(), 2);
+                    var valy = Math.Pow(double.Parse(args[1]) - coord.GetYCoord(), 2);
+                    double val = valx + valy;
+                    distance.Add(Math.Round(Math.Sqrt(val), 4));
+                }
+                for (int i = 0; i < distance.Count - 1; i++)
+                    for (int j = i + 1; j < distance.Count; j++)
+                    {
+                        if (distance[i] > distance[j])
+                        {
+                            double aux = distance[i];
+                            distance[i] = distance[j];
+                            distance[j] = aux;
+                            Coffee_Shop caf = coffee_shops[i];
+                            coffee_shops[i] = coffee_shops[j];
+                            coffee_shops[j] = caf;
+                        }
+                    }
+                int firstThreeShops = 0;
+                foreach (var caf in coffee_shops)
+                {
+                    Console.WriteLine(caf.GetName() + "   " + distance[firstThreeShops]);
+                    firstThreeShops++;
+                    if (firstThreeShops == 3) break;
+                }
             }
-
         }
     }
 }
-
